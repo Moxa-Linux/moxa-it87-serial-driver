276108c430748a684067a3620518b16ba9ed0672
Elvis Yao <elviscw.yao@moxa.com>
2021-09-08 17:13:51 +0800

    chore: bump module version to 1.4.0

--- it87_serial.c
+++ it87_serial.c
@@ -396,4 +396,4 @@ MODULE_SOFTDEP("pre: it87"); /* to avoid super IO drivers busy */
 MODULE_DESCRIPTION("Serial Port Register Control for IT8786 Super I/O chips");
 MODULE_AUTHOR("Remus Wu <remusty.wu@moxa.com>");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.3.0");
+MODULE_VERSION("1.4.0");
